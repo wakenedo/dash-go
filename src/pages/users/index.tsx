35b7cb17d627a0856be9871a05aa8bfaf41e9225
wakenedo <a_nuner@outlook.com>
2022-03-21 16:33:37 -0300

    Responsiveness - Part 4
    This time was the users Component the table was adapted to the smaller displays removing the edit and adapting the button to receive just the Icon, removed the row 'Registred at' and changed the flex direction of the pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+
     return (
         <Box>
             <Header />
@@ -19,7 +24,7 @@ export default function UserList() {
                             size='sm'
                             fontSize='sm'
                             colorScheme='pink'
-                            leftIcon={<Icon as={RiAddLine} fontSize='20'/>}
+                            leftIcon={<Icon as={RiAddLine} fontSize='20' />}
                         >
                             Create New User
                         </Button>
@@ -27,17 +32,17 @@ export default function UserList() {
                     <Table colorScheme='whiteAlpha'>
                         <Thead>
                             <Tr>
-                                <Th px='6' color='gray.300' width='8'>
+                                <Th px={['4', '4', '6']} color='gray.300' width='8'>
                                     <Checkbox colorScheme='pink' />
                                 </Th>
                                 <Th>User</Th>
-                                <Th>Registered At</Th>
+                                {isWideVersion && <Th>Registered At</Th>}
                                 <Th width='8'></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td>
+                                <Td px={['4', '4', '6']}>
                                     <Checkbox colorScheme='pink' />
                                 </Td>
                                 <Td>
@@ -46,18 +51,33 @@ export default function UserList() {
                                         <Text fontSize='small' color='gray.300'>a_nuner@outlook.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>april 04 2022</Td>
-                                <Td>
-                                    <Button
-                                        as='a'
-                                        size='sm'
-                                        fontSize='sm'
-                                        colorScheme='purple'
-                                        leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
-                                    >
-                                        Edit
-                                    </Button>
-                                </Td>
+                                {isWideVersion ? <Td>april 04 2022</Td> : ''}
+
+                                {isWideVersion ?
+                                    <Td>
+                                        <Button
+                                            as='a'
+                                            size='sm'
+                                            fontSize='sm'
+                                            colorScheme='purple'
+                                            leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                                        >
+                                            Edit
+                                        </Button>
+                                    </Td>
+                                    :
+                                    <Td>
+                                        <Button
+                                            as='a'
+                                            size='sm'
+                                            fontSize='sm'
+                                            colorScheme='purple'
+                                        >
+                                            <Icon as={RiPencilLine} fontSize='16' />
+                                        </Button>
+                                    </Td>
+                                }
+
                             </Tr>
                         </Tbody>
                     </Table>
