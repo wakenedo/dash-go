669a9946e055e0a47738071da42fa2019fb67cfe
wakenedo <a_nuner@outlook.com>
2022-03-22 11:40:50 -0300

    Routing - Part 1
    Just implemented the routes within the app, since it was all static. Now Sidebar links are redirecting to their designed endpoints. Also enabled the create button in users page , and the cancel button in the create.tsx

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 import { Input } from "../../components/Form/input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
@@ -9,24 +10,26 @@ export default function CreateUser() {
             <Header />
             <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
                 <Sidebar />
-                <Box flex='1' borderRadius={8} bg='gray.800' p={['6','8']}>
+                <Box flex='1' borderRadius={8} bg='gray.800' p={['6', '8']}>
                     <Heading size='lg' fontWeight='normal'>Create User</Heading>
                     <Divider my='6' borderColor='gray.700' />
 
                     <VStack spacing='8'>
-                        <SimpleGrid minChildWidth='240px' spacing={['6','8']} w='100%'>
+                        <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
                             <Input name='name' label='Full Name' />
                             <Input name='email' type='email' label='E-mail' />
                         </SimpleGrid>
-                        <SimpleGrid minChildWidth='240px' spacing={['6','8']} w='100%'>
+                        <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
                             <Input name='password' type='password' label='Password' />
                             <Input name='password_confirmation' type='password' label='Confirm your Password' />
                         </SimpleGrid>
                     </VStack>
 
-                    <Flex mt='8' justify='flex-end'> 
+                    <Flex mt='8' justify='flex-end'>
                         <HStack spacing='4'>
-                            <Button colorScheme='whiteAlpha'>Cancel</Button>
+                            <Link href='/users' passHref>
+                                <Button as='a' colorScheme='whiteAlpha'>Cancel</Button>
+                            </Link>
                             <Button colorScheme='pink'>Save</Button>
                         </HStack>
                     </Flex>
